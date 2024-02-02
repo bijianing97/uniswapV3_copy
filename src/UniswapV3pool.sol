be3ab6362e9518681ae50ddb38bba9d70793e4f2
bijianing97 <826015751@qq.com>
2024-02-02 17:23:24 +0800

    chore: add ui and bit lib

--- src/UniswapV3pool.sol
+++ src/UniswapV3pool.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.8.14;
 import "./lib/Tick.sol";
 import "./lib/Position.sol";
 import "./interfaces/IERC20.sol";
+import "./lib/TickBitmap.sol";
 import "./interfaces/IUniswapV3SwapCallback.sol";
 import "./interfaces/IUniswapV3MintCallback.sol";
 
 contract UniswapV3Pool {
     using Tick for mapping(int24 => Tick.Info);
+    using TickBitmap for mapping(int16 => uint256);
     using Position for mapping(bytes32 => Position.Info);
     using Position for Position.Info;
 
@@ -71,6 +73,63 @@ contract UniswapV3Pool {
         token0 = _token0;
         token1 = _token1;
         slot0 = Slot0({sqrtPriceX96: sqrtPriceX96, tick: tick});
+    }
+
+    function position(
+        int24 tick
+    ) private pure returns (int16 wordPos, uint8 bitPos) {
+        wordPos = int16(tick >> 8);
+        bitPos = uint8(uint24(tick % 256));
+    }
+
+    function flipTick(
+        mapping(int16 => uint256) storage self,
+        int24 tick,
+        int24 tickSpacing
+    ) internal {
+        require(tick % tickSpacing == 0); // ensure that the tick is spaced
+        (int16 wordPos, uint8 bitPos) = position(tick / tickSpacing);
+        uint256 mask = 1 << bitPos;
+        self[wordPos] ^= mask;
+    }
+
+    function nextInitializedTickWithOneWord(
+        mapping(int16 => uint256) storage self,
+        int24 tick,
+        int24 tickSpacing,
+        bool lte
+    ) internal view returns (int24 next, bool initialized) {
+        int24 compressed = tick / tickSpacing;
+
+        if (lte) {
+            (int16 wordPos, uint8 bitPos) = position(compressed);
+            uint256 mask = (1 << bitPos) - 1 + (1 << bitPos);
+            uint256 masked = self[wordPos] & mask;
+
+            initialized = masked != 0;
+
+            next = initialized
+                ? (compressed -
+                    int24(
+                        uint24(bitPos - BitMath.mostSignificantBit(masked))
+                    )) * tickSpacing
+                : (compressed - int24(uint24(bitPos))) * tickSpacing;
+        } else {
+            (int16 wordPos, uint8 bitPos) = position(compressed + 1);
+            uint256 mask = ~((1 << bitPos) - 1);
+            uint256 masked = self[wordPos] & mask;
+
+            initialized = masked != 0;
+            // overflow/underflow is possible, but prevented externally by limiting both tickSpacing and tick
+            next = initialized
+                ? (compressed +
+                    1 +
+                    int24(
+                        uint24((BitMath.leastSignificantBit(masked) - bitPos))
+                    )) * tickSpacing
+                : (compressed + 1 + int24(uint24((type(uint8).max - bitPos)))) *
+                    tickSpacing;
+        }
     }
 
     function mint(
